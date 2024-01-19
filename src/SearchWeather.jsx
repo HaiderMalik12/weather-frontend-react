f7e3ebc2c28c747728ede35cd1f9d84771dcb3fc
HaiderMalik12 <haidermalik504@gmail.com>
2024-01-19 18:11:46 +0500

    added hooks

--- src/SearchWeather.jsx
+++ src/SearchWeather.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 const SearchWeather = () => {
-  const location = "Faislabad";
+  const [location, setLocation] = useState("Faisalabad");
 
   return (
     <div>
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="Location"
+          />
         </label>
         <button>Submit</button>
       </form>
