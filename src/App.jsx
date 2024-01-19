a631eebfc6e609aaa367533fb377f4dc19892737
HaiderMalik12 <haidermalik504@gmail.com>
2024-01-19 18:55:52 +0500

    implement usestate to render weather details

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import { createRoot } from "react-dom/client";
 import SearchWeather from "./SearchWeather";
 import WeatherDetails from "./WeatherDetails";
 import ForeCastList from "./ForeCastList";
+import { useState } from "react";
 
 const App = () => {
+  const [todayWeather, setWeather] = useState({
+    city: "Faisalabad",
+    date: "02/19",
+    temprature: "48",
+    icon: "03d",
+    humidity: "",
+    wind: "",
+  });
+
+  //FETCH THE WEATHER DETAIL FROM THE BACKEND API
+  // SET THE WEATHER BY CALLING THE SETWEATHER
+  // RE-RENDER THE WEATHER DETAILS COMPONET HERE
   return (
     <div>
       <h1>Weather App</h1>
       <SearchWeather />
-      <WeatherDetails city="Faisalabad" temprature="28 Degree" />
+      <WeatherDetails
+        city={todayWeather.city}
+        temprature={todayWeather.temprature}
+      />
       <ForeCastList />
     </div>
   );
