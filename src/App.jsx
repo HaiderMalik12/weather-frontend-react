9786b1e31080c33d3bfbf59895f12751adcdf2c0
HaiderMalik12 <haidermalik504@gmail.com>
2024-01-23 23:24:41 +0500

    redesign the weather details section

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,15 @@ import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Forecast from "./Forecast";
 
 const App = () => {
   return (
     <>
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <h2 className="header">Weather App</h2>
-        </Container>
-      </Navbar>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SearchWeather />}></Route>
-          <Route path="/forecast/:location" element={<SearchWeather />}></Route>
+          <Route path="/" element={<SearchWeather />} />
+          <Route path="/forecast/:city" element={<Forecast />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/WeatherDetails.jsx
+++ src/WeatherDetails.jsx
@@ -1,6 +1,7 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 
 const WeatherDetails = ({
   city,
@@ -12,28 +13,30 @@ const WeatherDetails = ({
   wind,
 }) => {
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>
-          <h4>
-            {city} {temperature} &deg;{" "}
-          </h4>
-          <img src={icon} alt="Weather data by WeatherAPI.com" border="0" />
-        </Card.Title>
-        <Card.Text>
-          <Row>
-            <Col>
-              <p>{date}</p>
-              <p> {time}</p>
-            </Col>
-            <Col>
-              <p>Wind: {wind}</p>
-              <p>Humidity: {humidity}</p>
-            </Col>
-          </Row>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <Link to={`/forecast/${city}`} style={{ textDecoration: "none" }}>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            <h4>
+              {city} {temperature} &deg;{" "}
+            </h4>
+            <img src={icon} alt="Weather data by WeatherAPI.com" border="0" />
+          </Card.Title>
+          <Card.Text>
+            <Row>
+              <Col>
+                <p>{date}</p>
+                <p> {time}</p>
+              </Col>
+              <Col>
+                <p>Wind: {wind}</p>
+                <p>Humidity: {humidity}</p>
+              </Col>
+            </Row>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 
